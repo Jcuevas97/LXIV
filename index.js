206de2147b0fd1e54f58984ee0c75e40f440f3a8
Johnny Cuevas <jcuevas@cristoreyny.org>
2015-05-09 17:45:17 +0000

    the javascript comencement

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+
+
+
+var board=[[0,1,2,3],
+           [0,0,2,0],
+           [0,1,0,3],
+           [0,1,2,0]];
 document.onkeyup = checkKey;
 
 function checkKey(e) {
@@ -16,5 +23,36 @@ function checkKey(e) {
     else if (e.keyCode == '39') {
        // right arrow
     }
-
+}
+function blankslate(){
+    return [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+}
+function transpose(board) {
+     var tboard=blankslate();
+    for (var row = 0; row < board.length; row++) {
+        for (var col = 0; col < board[row].length; col++) {
+         tboard[row][col]=board[col][row];
+        }
+    }
+    return tboard;
+}
+function reverse(board) {
+     var rboard=blankslate();
+    for (var row = 0; row < board.length; row++) {
+        for (var col = 0; col < board[row].length; col++) {
+         rboard[row][col]=board[row][board[row].length-row-1];
+        }
+    }
+    return rboard;
+}
+function rotateclock(board){
+    return reverse(transpose(board));
+}
+function moveup(board,row,col){
+    if();
+    if else();
+    
+    
+    
+    
 }
