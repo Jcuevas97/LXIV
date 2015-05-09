f0344ef54dcdddf7485ead579968a587c2be4fff
Johnny Cuevas <jcuevas@cristoreyny.org>
2015-05-09 18:59:01 +0000

    debugged

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function reverse(board) {
      var rboard=blankslate();
     for (var row = 0; row < board.length; row++) {
         for (var col = 0; col < board[row].length; col++) {
-         rboard[row][col]=board[row][board[row].length-row-1];
+         rboard[row][col]=board[row][board[row].length-col-1];
         }
     }
     return rboard;
@@ -51,72 +51,107 @@ function rotateclock(board){
 
 function moveup(board, row, col){
     for (var i = row; i >= 1; i--) {
-        if(board[row-1][col]==0){
-            board[row-1][col]=board[row][col]
-            board[row][col]=0
+        if(board[i-1][col]==0 && board[i][col] != 0){
+            board[i-1][col]=board[i][col]
+            board[i][col]=0
         }
     }
 }
 function moveallup(board){
-    for (var row=1; row<board.length; row++){
-        for (var col=0; col<board[row].length; col++){
+    for (var col=0; col<board[0].length; col++){
+        for (var row=1; row<board.length; row++){
             moveup(board, row, col);
         }
     }
 }
 function merge(board){
-    for (var row=1; row<board.length; row++){
-        for (var col=0; col<board[row].length; col++){
+    for (var col=0; col<board[0].length; col++){
+        for (var row=1; row<board.length; row++){
             if(board[row-1][col]==board[row][col]){
-                board[row-1][col]=board[row-1][col]*2
-                board[row][col]=0
+                board[row-1][col]=board[row-1][col]*2;
+                board[row][col]=0;
             }
         }
     }
 }
+
+// boad = some nonesense
+// handle(board, UP)
+// print(board)
+//
+//
 function handle(board,key){
-    if (e.keyCode == '38') {
+    if (key == '38') {
         // up arrow
-        moveup(board);
+        moveallup(board);
         merge(board);
-        moveup(board);
+        moveallup(board);
     }
-    else if (e.keyCode == '40') {
+    else if (key == '40') {
         // down arrow
         board=rotateclock(board);
         board=rotateclock(board);
-        moveup(board);
+        moveallup(board);
         merge(board);
-        moveup(board);
+        moveallup(board);
         board=rotateclock(board);
         board=rotateclock(board);
     }
-    else if (e.keyCode == '37') {
+    else if (key == '37') {
        // left arrow
         board=rotateclock(board);
-        moveup(board);
+        moveallup(board);
         merge(board);
-        moveup(board);
+        moveallup(board);
         board=rotateclock(board);
         board=rotateclock(board);
         board=rotateclock(board);
     }
-    else if (e.keyCode == '39') {
+    else if (key == '39') {
        // right arrow
         board=rotateclock(board);
         board=rotateclock(board);
         board=rotateclock(board);
-        moveup(board);
+        moveallup(board);
         merge(board);
-        moveup(board);
+        moveallup(board);
         board=rotateclock(board);
     }
+    addrand(board);
+    return board;
 }
 
 function addrand(board){
+    var count= 0;
+    for (var row=0; row<board.length; row++){
+        for (var col=0; col<board[row].length; col++){
+    if(board[row][col]==0){
+        count++;
+    }
     
     
     
-    
+        }
+    }
+    if (count==0){
+        return;
+    }
+    var thenum= Math.floor((Math.random()*count)+1);
+    count=0;
+    for (var row=0; row<board.length; row++){
+        for (var col=0; col<board[row].length; col++){
+             if(board[row][col]==0){
+        count++;
+        if(count==thenum){
+            board[row][col]=Math.floor(Math.random()*2)+1;
+            return;
+        }
+    }
+        }
+    }
     
 }
+
+
+
+
